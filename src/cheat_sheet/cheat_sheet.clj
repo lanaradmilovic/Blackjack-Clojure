50b06c5324cf775454b4b7a386b706ece99fb0fc
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2023-12-23 17:09:01 +0100

    Getters for cheat-sheet rows and columns generated

--- src/cheat_sheet/cheat_sheet.clj
+++ src/cheat_sheet/cheat_sheet.clj
@@ -7,3 +7,27 @@
                                               "22" "33" "44" "55" "66" "77" "88" "99" "1010" "AA"]
                               :dealers-card  ["2" "3" "4" "5" "6" "7" "8" "9" "10" "A"]
                               :values        (vec (repeat row-num (vec (repeat col-num "H"))))}))
+
+(defn- get-rows
+  [cheat-sheet]
+  (:players-cards cheat-sheet))
+(defn- get-cols
+  [cheat-sheet]
+  (:dealers-card cheat-sheet))
+(def row-names (get-rows @blackjack-cheat-sheet))
+(def col-names (get-cols @blackjack-cheat-sheet))
+(defn- value-for-ch
+  "Returns a cheat-sheet representation of the card value."
+  [val]
+  (case val
+    "ace" "A"
+    "jack" "10"
+    "queen" "10"
+    "king" "10"
+    val))
+(defn- get-row-idx
+  [row-name cheat-sheet]
+  (.indexOf (:players-cards cheat-sheet) row-name))
+(defn- get-col-idx
+  [col-name cheat-sheet]
+  (.indexOf (:dealers-card cheat-sheet) col-name))
