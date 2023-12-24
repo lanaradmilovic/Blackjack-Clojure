0de4fb2994d25f4c16565db8aa626b0c64dcdd0f
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2023-12-24 15:05:47 +0100

    Midje dependency added.

---
+++ test/cheat_sheet_test/cheat_sheet_test.clj
@@ -0,0 +1,34 @@
+(ns cheat-sheet-test.cheat-sheet-test
+  (:require [midje.sweet :refer :all]
+            [cheat-sheet.cheat-sheet :refer :all :as cs]))
+
+(fact "Testing cheat sheet values conversion."
+  (cs/value-for-ch "ace") => "A"
+  (cs/value-for-ch "jack") => "10"
+  (cs/value-for-ch "2") => "2"
+  (cs/value-for-ch "unknown") => "unknown")
+
+(fact "'update-sheet' should update value at a given position."
+      (let [initial-cheat-sheet (atom
+                                  {:players-cards ["8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "A2" "A3" "A4" "A5" "A6" "A7" "A8" "A9"
+                                                   "22" "33" "44" "55" "66" "77" "88" "99" "1010" "AA"]
+                                   :dealers-card  ["2" "3" "4" "5" "6" "7" "8" "9" "10" "A"]
+                                   :values        (vec (repeat 28 (vec (repeat 10 "H"))))})]
+        (get-in (:values (cs/update-sheet @initial-cheat-sheet 2 3 "new-value")) [2 3]) => "new-value"))
+
+(fact "Testing exception handling in case of non-existing player's and dealer's card."
+      (let
+        [initial-cheat-sheet (atom
+                                 {:players-cards ["8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "A2" "A3" "A4" "A5" "A6" "A7" "A8" "A9"
+                                                  "22" "33" "44" "55" "66" "77" "88" "99" "1010" "AA"]
+                                  :dealers-card  ["2" "3" "4" "5" "6" "7" "8" "9" "10" "A"]
+                                  :values        (vec (repeat 28 (vec (repeat 10 "H"))))})]
+        (cs/get-move initial-cheat-sheet "7" "1") => falsey))
+
+(let
+  [blackjack-cheat-sheet (atom
+                           {:players-cards ["8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "A2" "A3" "A4" "A5" "A6" "A7" "A8" "A9"
+                                            "22" "33" "44" "55" "66" "77" "88" "99" "1010" "AA"]
+                            :dealers-card  ["2" "3" "4" "5" "6" "7" "8" "9" "10" "A"]
+                            :values        (vec (repeat 10 (vec (repeat 5 "H"))))})]
+  )
