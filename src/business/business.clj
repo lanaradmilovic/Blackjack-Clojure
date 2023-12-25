0f3e22216159965243bbb465a83a151cd2d27638
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2023-12-25 13:57:33 +0100

    Functions for converting card values according to BJ rules and getters for card values added.

--- src/business/business.clj
+++ src/business/business.clj
@@ -62,12 +62,14 @@
       [face suit])))
 
 (defn add-new-player-card
+  "Adds new card to player's starting hand."
   [c new-c]
   (assoc c (keyword (str "card-" (inc (count c)))) new-c))
 (defn add-new-player-card!
   [c new-c]
   (swap! c add-new-player-card {:value (nth new-c 0) :suit (nth new-c 1)}))
 (defn add-new-current-card
+  "Adds new card to current game session cards."
   [c new-c]
   (update c :player-cards (fn
                                [existing-cards]
@@ -78,7 +80,63 @@
   (swap! c add-new-current-card {:value (nth new-c 0) :suit (nth new-c 1)}))
 
 (defn add-both
+  "Adds new card to both the player's starting hand and current game session cards."
   [c p]
   (let [new-c (read-new-card)]
     (add-new-player-card! p new-c)
     (add-new-current-card! c new-c)))
+
+(defn get-player-value
+  [c n]
+  (:value (get-nth-player-card c n)))
+
+(defn get-dealer-value
+  [c]
+  (:value (get-dealer-card c)))
+
+(defn card-value
+  "Converts a card's value into an Integer according to Blackjack rules."
+  [value]
+  (case (clojure.string/trim (clojure.string/lower-case value))
+    "jack" 10
+    "queen" 10
+    "king" 10
+    "ace" 11
+    (if (string? value)
+      (try
+        (Integer/parseInt value)
+        (catch NumberFormatException _ (throw (IllegalArgumentException. "Invalid card value."))))
+      value)))
+
+(defn convert-card-value
+  ([c n]
+   (let [value (get-player-value c n)]
+             (card-value value)))
+  ([c]
+   (let [value (get-dealer-value c)]
+     (card-value value))))
+
+(defn player-values-for-cheat-sheet
+  [hand]
+  "Returns player cards with converted value by calling 'convert-card-value'."
+  (loop [i 1 n (count hand) values hand]
+    (if (> i n)
+      values
+      (recur (inc i) n (assoc-in values [(keyword (str "card-" i)) :value] (convert-card-value hand i))))))
+(defn dealer-values-for-cheat-sheet
+  [hand]
+  "Returns dealer card with converted value by calling 'convert-card-value'."
+  (assoc (get-dealer-card hand) :value (convert-card-value hand)))
+
+(defn get-player-values
+  [hand]
+  "Returns list of original player cards values."
+  (loop [i 1 n (count hand) values '()]
+    (if (> i n)
+      values
+      (recur (inc i) n (conj values (get-player-value hand i))))))
+(defn get-all-values
+  [hand]
+  "Returns list of both dealer and player original cards values."
+  (concat (map #(get % :value) (:player-cards hand))
+          [(str (:value (first (:dealer-card hand))))]))
