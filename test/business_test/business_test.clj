687431aad73f0c57b111658e851a6d841f8fc9ad
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2023-12-27 01:34:10 +0100

    Tests for sum of player cards and adjusting ace value added.

--- test/business_test/business_test.clj
+++ test/business_test/business_test.clj
@@ -1,11 +1,11 @@
 (ns business-test.business-test
   (:require [midje.sweet :refer :all]
             [business.business :refer :all :as b]))
-  (fact "Testing generate-card function"
-        (let [card (generate-card "10" "heart")]
-          (fact "Generated card should have the correct value and suit"
-                (card :value) => "10"
-                (card :suit) => "heart")))
+(fact "Testing generate-card function"
+      (let [card (generate-card "10" "heart")]
+        (fact "Generated card should have the correct value and suit"
+              (card :value) => "10"
+              (card :suit) => "heart")))
 
 (fact "Testing 'get-nth-player-card' function"
       (fact "When card has the nth player card"
@@ -86,10 +86,71 @@
 (fact "Updating player card value."
       (let [player-cards (atom {:card-1 {:value "10" :suit "heart"}
                                 :card-2 {:value "10" :suit "heart"}})]
-       (fact "Updating player card value with a non-existing card number should return nil."
-             (b/update-player-card-value! player-cards 3 "5") => falsey)
+        (fact "Updating player card value with a non-existing card number should return nil."
+              (b/update-player-card-value! player-cards 3 "5") => falsey)
         (fact "'update-player-card-value!' should return 'nil' when provided with dereferenced values."
               (b/update-player-card-value! @player-cards 3 "5") => falsey)))
+
+(fact "Calculating sum of player cards"
+      (let [empty-hand {}
+            single-card {:card-1 {:value "10" :suit "heart"}}]
+
+        (fact "With an empty hand should return 0."
+              (player-sum empty-hand) => 0)
+
+        (fact "With a single card should return the value of that card."
+              (player-sum single-card) => 10)))
+
+(fact "'get-ace-position' and 'get-no-ace-position' should handle different scenarios"
+      (let [empty-hand {}
+            hand-without-ace {:card-1 {:value "king" :suit "heart"}
+                              :card-2 {:value "queen" :suit "diamond"}}
+            hand-with-ace {:card-1 {:value "ace" :suit "heart"}
+                           :card-2 {:value "king" :suit "diamond"}
+                           :card-3 {:value "ace" :suit "spade"}}]
+        (fact "Return an empty list for an empty hand."
+              (get-ace-position empty-hand) => []
+              (get-no-ace-position empty-hand) => [])
+
+        (fact "Return an empty list for a hand with no 'ace' values."
+              (get-ace-position hand-without-ace) => []
+              (get-no-ace-position hand-without-ace) => [2 1])
+
+        (fact "Return the positions of cards with 'ace' value."
+              (get-ace-position hand-with-ace) => [3 1]
+              (get-no-ace-position hand-with-ace) => [2])))
+
+(fact "'adjust-ace-value' should handle different scenarios"
+      (let [no-ace-hand (atom {:card-1 {:value "king" :suit "heart"}
+                               :card-2 {:value "queen" :suit "diamond"}})
+            two-ace-hand (atom {:card-1 {:value "ace" :suit "heart"}
+                                :card-2 {:value "ace" :suit "diamond"}})
+            ace-value-eleven (atom {:card-1 {:value "ace" :suit "heart"}
+                                       :card-2 {:value "king" :suit "diamond"}})
+            ace-value-one (atom {:card-1 {:value "ace" :suit "heart"}
+                                     :card-2 {:value "king" :suit "diamond"}
+                                     :card-3 {:value "ace" :suit "spade"}})]
+
+        (fact "No adjustment is made for a hand without 'ace' cards."
+              (adjust-ace-value! no-ace-hand) => no-ace-hand)
+
+        (fact "No adjustment is made when the sum is less or equals to 21."
+              (adjust-ace-value! no-ace-hand) => no-ace-hand
+              (adjust-ace-value! ace-value-eleven) => ace-value-eleven)
+
+        (fact "Adjust 'ace' value from 11 to 1 when the sum is over 21."
+              (adjust-ace-value! ace-value-one) => {:card-1 {:value 1 :suit "heart"}
+                                                        :card-2 {:value "king" :suit "diamond"}
+                                                        :card-3 {:value 1 :suit "spade"}}
+              (deref (adjust-ace-value! two-ace-hand)) => {:card-1 {:value "ace" :suit "heart"}
+                                                           :card-2 {:value 1 :suit "diamond"}})))
+
+
+
+
+
+
+
 
 
 
