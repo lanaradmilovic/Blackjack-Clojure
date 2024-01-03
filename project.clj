3674a72a566a287603b63c72cbde3833a626e8f1
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-01-03 01:06:26 +0100

    Dependency for jdbc and h2 database added.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [midje "1.9.3"]]
+                 [midje "1.9.3"]
+                 [seancorfield/next.jdbc "1.2.659"]
+                 [com.h2database/h2 "1.4.200"]]
   :main ^:skip-aot blackjack.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
