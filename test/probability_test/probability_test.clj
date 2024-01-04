f3cd59ea89a595b0bc9a0fa7113e03e1c22fda78
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-01-04 11:55:38 +0100

    Refactoring probability_test.clj test code because probability.clj is deleted.

--- test/probability_test/probability_test.clj
+++ test/probability_test/probability_test.clj
@@ -1,46 +1,8 @@
 (ns probability-test.probability-test
   (:require [midje.sweet :refer :all]
-            [probability.probability :refer :all :as p]))
+            [business.business :refer :all :as p]))
 
-(fact "Testing contain-element? function"
-      (fact "Testing contain-element? function with existing element"
-            (p/contain-element? [1 2 3] 2) => true)
 
-      (fact "Testing contain-element? function with non-existing element"
-            (p/contain-element? [1 2 3] 4) => falsey)
-
-      (fact "Testing contain-element? function with empty list"
-            (p/contain-element? [] 1) => falsey)
-
-      (fact "Testing contain-element? function with duplicate elements"
-            (p/contain-element? [1 2 2 3] 2) => true))
-
-(fact "Testing 'generate-list' function"
-      (fact "Testing generate-list function with positive n"
-            (generate-list 5) => (reverse '(1 2 3 4 5)))
-
-      (fact "Testing generate-list function with n = 1"
-            (generate-list 1) => '(1))
-
-      (fact "Testing generate-list function with n = 0"
-            (generate-list 0) => '())
-
-      (fact "Testing generate-list function with negative n"
-            (generate-list -3) => '())
-
-      (fact "Testing generate-list function with large n"
-            (generate-list 1000) => (reverse (take 1000 (iterate inc 1)))))
-
-(fact "Testing 'get-all-val' function"
-      (fact "Testing get-all-val function with a player and dealer hand"
-            (get-all-val {:player-cards (list {:value "4" :suit "heart"} {:value "4" :suit "heart"})
-                          :dealer-card  (list {:value "king" :suit "heart"})})
-            => '("4" "4" "king"))
-
-      (fact "Testing get-all-val function with an empty hand"
-            (get-all-val {:player-cards '()
-                          :dealer-card  nil})
-            => '("")))
 
 (fact "Testing 'decrement-counter-on-match' function"
       (fact "Testing decrement-counter-on-match function with matching values"
